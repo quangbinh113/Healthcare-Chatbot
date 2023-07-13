b3509b57840c8010ca322937db5300daa2ba030f
Luong <3q04032002@gmail.com>
2023-07-13 23:03:53 +0700

    fix bug

--- DialoGPT/test.py
+++ DialoGPT/test.py
@@ -20,7 +20,7 @@ with open(args.config, "rb") as f:
     config = json.load(f)
 
 tokenizer = AutoTokenizer.from_pretrained('microsoft/DialoGPT-small')
-tokenizer.pad_token = tokenizer.eos_token
+tokenizer.pad_token_id = tokenizer.eos_token_id
 model = AutoModelForCausalLM.from_pretrained("microsoft/DialoGPT-small")
 
 
